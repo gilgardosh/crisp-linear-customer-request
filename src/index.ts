55b89b86cc1526136b1734ad67111330b890605c
gilgardosh <gilgardosh@gmail.com>
2025-01-27 18:56:23 +0200

    next steps

--- src/index.ts
+++ src/index.ts
@@ -40,6 +40,38 @@ async function main() {
     if (meta.email) {
       const domain = meta.email.split("@")[1];
       console.log("Domain:", domain);
+
+      // figure out customer id out of domain
+
+      // figure out project, team id out of content / segments
+
+      // figure out issue status
+
+      // wrap conversation content into a Linear customer need
+
+      // send the Linear customer need
+
+      /**
+       * Documentation of the Linear part:
+       * https://linear.app/docs/customer-requests?noRedirect=1&tabs=5bb93b53f1eb#integrations
+       * 
+       * Schema link:
+       * https://studio.apollographql.com/public/Linear-API/variant/current/explorer?searchQuery=Mutation.customerNeedCreate
+       * 
+       * "input": {
+       *   "attachmentId": null,
+       *   attachmentUrl": null,
+       *   body": null,
+       *   bodyData": null,
+       *   commentId": null,
+       *   customerExternalId": null,
+       *   customerId": null,
+       *   id": null,
+       *   issueId": null,
+       *   priority": null,
+       *   projectId": null
+       * }
+       *  */
     }
   })
     .then(() => {
